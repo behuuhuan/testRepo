d0e0b55b5d5640b1fc2bd90c8f803dc5a0f2e232
Phi Tran The <phitt1@fsoft.com.vn>
2017-08-07 10:57:47 +0700

    commit new code for branch Test1

--- src/main/java/com/test/testrepo/MyApp.java
+++ src/main/java/com/test/testrepo/MyApp.java
@@ -2,7 +2,8 @@ package com.test.testrepo;
 
 public class MyApp {
 	public static void main(String[] args) {
-		System.out.println("XD");
+		int a = 10;
+		System.out.println(a);
 		System.out.println("XXXD");
 	}
 }
